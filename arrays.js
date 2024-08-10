b46883709b81db2826f7415aee02ff5cd736838c
boris <boriskirenga@gmail.com>
2024-08-10 09:20:39 -0700

    a file of arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const fruits = [ 'apples','oranges','pears'];
+fruits [3] = 'grapes';
+fruits.push('mangos');
+fruits.unshift('strawberries')
+fruits.pop(),
+console.log(Array.isArray('hello'));
+console.log(fruits.indexOf('pears'));
+console.log(fruits);
