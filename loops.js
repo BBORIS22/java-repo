368325c317288bc4869af9a8849777e68694443a
boris <boriskirenga@gmail.com>
2024-08-12 09:57:13 -0700

    added a file of loops and updated an index.html

---
+++ loops.js
@@ -0,0 +1,11 @@
+// for
+for( let i = 0 ; i <= 10 ; i++) {
+console.log(`for loop number : ${i}`);
+}
+
+// while
+// let i = 0
+// while (i < 10) {
+//     console.log(`for loop number : ${i}`);
+//     i++;
+// }
