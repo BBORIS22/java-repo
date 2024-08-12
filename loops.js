b95f777b86c94f7d7cfe7bdb9fc5b26e8fffbeaf
boris <boriskirenga@gmail.com>
2024-08-12 12:08:56 -0700

    added a plus on keys of objects on line 14

--- loops.js
+++ loops.js
@@ -10,13 +10,14 @@ console.log(`for loop number : ${i}`);
 //     i++;
 // }
 
+
 let codes = {
-    "49": "Germany",
-    "41": "Switzerland",
-    "44": "Great Britain",
-    "1": "USA"
+    "+49": "Germany",
+    "+41": "Switzerland",
+    "+44": "Great Britain",
+    "+1": "USA"
   };
 
   for (let code in codes) {
-    console.log(codes[code]);
+    console.log(+code);
   }
