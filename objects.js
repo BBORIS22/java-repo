1eb0a90b51d5062dcc2bcc60ebd3e153d82effcb
boris <boriskirenga@gmail.com>
2024-08-12 10:51:04 -0700

    added a demo for loop in the file

--- objects.js
+++ objects.js
@@ -15,3 +15,12 @@ const person = {
 
 person.email ='john@gmail.com';
 console.log (person);
+
+let user = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+  };
+  for (let key in user) {
+    console.log(user[key]);
+  }
