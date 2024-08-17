8fde37abcec0f30614bce7e0ca76c911681e2be6
boris <boriskirenga@gmail.com>
2024-08-16 22:39:52 -0700

    added plus again on codes on line 15

--- loops.js
+++ loops.js
@@ -12,12 +12,12 @@ console.log(`for loop number : ${i}`);
 
 
 let codes = {
-    "49": "Germany",
-    "41": "Switzerland",
-    "44": "Great Britain",
-    "1": "USA"
+    "+49": "Germany",
+    "+41": "Switzerland",
+    "+44": "Great Britain",
+    "+1": "USA"
   };
 
   for (let code in codes) {
-    console.log(code);
+    console.log(+code);
   }
