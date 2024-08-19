b6129aa2cdf2c848e9f0ed720adce789c3afaaa8
boris <boriskirenga@gmail.com>
2024-08-19 13:12:53 -0700

    updated the file

--- ternaryop.js
+++ ternaryop.js
@@ -3,4 +3,14 @@ const x = 10;
 
 const color = x > 10 ? 'red' : 'blue';
 
-console.log (color);
+// console.log (color);
+switch(color){
+    case 'blue': 
+    console.log('color is blue');
+    break;
+    case 'red':
+        console.log('color is red');
+        break;
+        default:
+            console.log('color is not red or blue');
+}
