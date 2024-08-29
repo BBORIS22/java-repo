f4026f8c8b5b94852a53f13a37bc260a4c042102
boris <boriskirenga@gmail.com>
2024-08-29 13:50:43 -0700

    modified my file with styles for the interface

--- mydom.js
+++ mydom.js
@@ -15,10 +15,19 @@ const ul = document.querySelector('.items');
 
 // ul.remove();
 // ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'hello';
-ul.children[1].innerText = 'boris';
-ul.lastElementChild.innerHTML = '<h1>item3<h1\>'
+// ul.firstElementChild.textContent = 'hello';
+// ul.children[1].innerText = 'boris';
+// ul.lastElementChild.innerHTML = '<h1>item3<h1\>'
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
 
 const btn = document.querySelector('.btn');
-btn.style.background = 'red';
 
+btn.addEventListener('click',(e) =>{
+    e.preventDefault();
+document.querySelector('#my-form').style.background = 
+'#ccc';
+document.querySelector('body').classList.add('bg-dark');
+document.querySelector('.items').lastElementChild.innerHTML = '<h1>hello<\h1>'
+});
